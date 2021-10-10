5bfe0e794f383bcfbd1b6e6d9c50dbfa601b5139
kota-kota <ko.lespaul0119@gmail.com>
2021-10-10 22:11:23 +0900

    拡張機能「Copy with Line Number」にCopy without TABを追加

--- copy-with-line-number/README.md
+++ copy-with-line-number/README.md
@@ -9,8 +9,14 @@
 行の先頭に行番号を付与してテキストコピー
 
 - コピーしたいテキストを選択する。
-- `Ctrl+Alt+C` を入力する。
+- `Ctrl+Alt+L` を入力する。
 - または、コマンドパレット `F1` から `Copy with Line Number: Copy` を選択する。
+
+行の先頭に行番号を付与＋タブ文字を空白4文字に変換してテキストコピー
+
+- コピーしたいテキストを選択する。
+- `Ctrl+Alt+T` を入力する。
+- または、コマンドパレット `F1` から `Copy with Line Number: Copy without TAB` を選択する。
 
 ## Requirements
 
--- copy-with-line-number/src/extension.ts
+++ copy-with-line-number/src/extension.ts
@@ -10,39 +10,61 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "copy-with-line-number" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('copy-with-line-number.copy', () => {
-		let editor = vscode.window.activeTextEditor;	//エディタ取得
-		let sel = editor?.selection;	//選択範囲取得
-		let slineNo = sel?.start.line;	//選択範囲先頭行の行番号
-		if(slineNo == undefined) { return; }
-		let elineNo = sel?.end.line;	//選択範囲末尾行の行番号
-		if(elineNo == undefined) { return; }
-		let digit = String(elineNo).length;	//末尾行番号の桁数
+	// copy-with-line-number.copy
+	context.subscriptions.push(vscode.commands.registerCommand('copy-with-line-number.copy', () => {
+		let textArray : string[] = getCopyTextWithLineNumber();
+		let text : string = '';
 
-		// 先頭行から末尾行まで
-		let textWithLine = '';
-		for(let l = slineNo; l <= elineNo; l++) {
-			// 指定行のテキストを取得
-			let text = editor?.document.lineAt(l).text;
-
-			// 実際の行番号は+1した値
-			let lineNo = l + 1;
-
-			// 行番号の文字列. 桁数を合わせるために桁数分空白で右寄せフォーマット.
-			let line = (Array(digit).fill(' ').join('') + lineNo).substr(-1 * digit);
-			textWithLine += line + ': ' + text + '\n';
+		for (let t of textArray) {
+			text += t + '\n';
 		}
 
 		// クリップボードに書き込み
-		vscode.env.clipboard.writeText(textWithLine);
-		vscode.window.showInformationMessage('Copy! line:' + slineNo + '.' + elineNo);
-	});
+		vscode.env.clipboard.writeText(text);
+		vscode.window.showInformationMessage('Copy with line number!');
+	}));
 
-	context.subscriptions.push(disposable);
+	// copy-with-line-number.copy-without-tab
+	context.subscriptions.push(vscode.commands.registerCommand('copy-with-line-number.copy-without-tab', () => {
+		let textArray : string[] = getCopyTextWithLineNumber();
+		let text : string = '';
+
+		for (let t of textArray) {
+			text += t.replace(/\t/g, "    ") + '\n';
+		}
+
+		// クリップボードに書き込み
+		vscode.env.clipboard.writeText(text);
+		vscode.window.showInformationMessage('Copy with line number, without TAB!');
+	}));
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+// コピーした範囲のテキストを行番号付きに加工して文字列リストで返す
+function getCopyTextWithLineNumber() : string[] {
+	let text : string[] = [];
+
+	let editor = vscode.window.activeTextEditor;	//エディタ取得
+	let sel = editor?.selection;	//選択範囲取得
+	let slineNo = sel?.start.line;	//選択範囲先頭行の行番号
+	if(slineNo == undefined) { return text; }
+	let elineNo = sel?.end.line;	//選択範囲末尾行の行番号
+	if(elineNo == undefined) { return text; }
+	let digit = String(elineNo).length;	//末尾行番号の桁数
+
+	// 先頭行から末尾行まで
+	for(let l = slineNo; l <= elineNo; l++) {
+		// 指定行のテキストを取得
+		let t = editor?.document.lineAt(l).text;
+
+		// 実際の行番号は+1した値
+		let lineNo = l + 1;
+
+		// 行番号の文字列. 桁数を合わせるために桁数分空白で右寄せフォーマット.
+		let line = (Array(digit).fill(' ').join('') + lineNo).substr(-1 * digit);
+		text.push(line + ': ' + t);
+	}
+	return text;
+}
